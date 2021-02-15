8847d2d5671005ed3b78d684a06288ce13eae188
garegetan <79066977+garegetan@users.noreply.github.com>
2021-02-15 08:20:59 +0700

    Create tutorialan.sh

---
+++ tutorialan.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+green='\33[32;1m'
+blue='\33[0;36'
+red='\33[31;1m'
+purple='\e[0;35m'
+Oren='\33[33;1m'
+black='\33[30;1m'
+white='\33[37;1m'
+
+clear
+
+toilet -f mini -F gay Fauzan Tools
+echo $green "++++++++++++++++++++++++++++"
+echo "KONTAK :081276754581"
+echo "CREATOR BY : FAUZAN PRATAMA"
+echo "MEMBUAT STABIL JARINGAN"
+echo $Oren"============================"
+echo
+echo $white"[ PILIH MENU ]"
+echo "[1]STABILKAN JARINGAN"
+echo "[2]HACK WA"
+echo "[3]HACK FB"
+echo "[4]BUG MAXSTREAM"
+echo "[5]EXIT"
+echo
+read -p "[ PILIH NOMOR ]>>>" pil ;
+if [ $pil = "1" ]
+    then
+    clear
+    toilet -f mini -F gay Stabil Ping
+      echo "WAIT LAGI PROSES ..."
+      sleep 2
+      pkg update && pkg upgrade -y
+      ping -s 1000 8.8.8.8
+echo "Proses selesai..."
+       echo "WAIT LAGI PROSES ..."
+       fi
+      if [ $pil = "2" ]
+    then
+    clear
+    toilet -f mini -F gay Hack wa
+      echo "WAIT LAGI PROSES ..."
+      sleep 2
+      pkg update && pkg upgrade -y
+      pkg update && pkg upgrade
+pkg install git
+pkg install curl
+git clone https://github.com/AndriGanz/whatshack
+cd whatshack
+ls
+sh whatshack.sh
+echo "Proses dihentikan..."
+      fi
+      if [ $pil = "3" ]
+      then
+      clear
+      toilet -f mini -F gay Hack Fb
+        echo $green"Loading..."
+        apt update && apt upgrade
+pkg ins toilet
+pkg ins figlet
+pkg ins ruby
+pkg ins gem
+gem ins lolcat
+pkg ins php
+pkg ins python2
+pkg ins git
+git clone https://github.com/Rusmana-ID/rus,
+cd rus
+sh v2.sh
+        echo "Success !!!"
+        fi
+        if [ $pil = "4" ]
+       then
+       clear
+       toilet -f mini -F gay Bug maxstream
+       sleep 2
+       play.hbogo.eu
+       echo "bug maxstream sukses..."
+    elif [ $pil = "5" ]
+    then
+      echo "LAGI KELUAR NGAB..."
+      sleep 2
+      exit
+    else
+      echo "MODULE SALAH BEGO"
+      sleep 2
+echo $ulang
+    fi
+done
